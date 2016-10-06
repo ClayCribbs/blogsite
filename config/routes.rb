911ab1b75a55d95398475d2e6e1c6b449741a546
Clay Cribbs <lclay.cribbs@gmail.com>
2016-10-06 16:46:20 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :blog_posts
+
+  root 'blog_posts#index'
+  post 'create_comment' => 'blog_posts#create_comment'
+  get 'delete_comment' => 'blog_posts#delete_comment'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
